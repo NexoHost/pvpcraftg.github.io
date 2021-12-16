6586575a1793d111c09c2114ca8e4d55efcf5241
Zastinian <82119938+Zastinian@users.noreply.github.com>
2021-12-16 16:28:09 -0600

    Se añadio todo

---
+++ src/js/smoothscroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 150
+	    }, 1100, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
